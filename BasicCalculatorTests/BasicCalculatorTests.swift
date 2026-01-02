cdadf9137cec9f53fa42752095f5d305d334d193
Harjot Singh <Harjotmatharu16@gmail.com>
2026-01-01 22:31:01 -0700

    Initial Commit

---
+++ BasicCalculatorTests/BasicCalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  BasicCalculatorTests.swift
+//  BasicCalculatorTests
+//
+//  Created by Harjot Singh on 1/1/26.
+//
+
+import Testing
+@testable import BasicCalculator
+
+struct BasicCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
